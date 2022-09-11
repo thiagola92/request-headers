2887f1766840bb7e34e7a8a0ccbc256f678234d6
thiagola92 <thiagola92@gmail.com>
2022-09-11 17:05:01 -0300

    Change to PDM and add github action

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
-import os
 import json
+import os
+import sys
+
 from quart import Quart, request
-from request_headers.mongo import insert_headers
+
 from request_headers.headers import clean_headers
+from request_headers.mongo import insert_headers
 
 app = Quart(__name__)
 
@@ -15,6 +18,11 @@ async def main():
     insert_headers(headers)
 
     return json.dumps(headers)
+
+
+@app.route("/exit")
+async def exit():
+    sys.exit(0)
 
 
 app.run(host="0.0.0.0", port=os.environ.get("PORT", 5000))
